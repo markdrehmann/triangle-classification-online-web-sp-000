67158173355fd4c1431bd68f3fa443a52084eb4d
Mark Drehmann <markdrehmann@gmail.com>
2020-03-14 22:38:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,13 @@
 class Triangle
-  def initialize(side_1, side_2, side_3)
+  attr_accessor :s1, :s2, :s3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def valid_triangle?
+    @s1 > 0 && @s2 > 0 && @s3 > 0 && @s1 + @s2 > @s3 && @s2 + @s3 > @s1 && @s1 + @s3 > @s2
   end
 
   class TriangleError <StandardError
